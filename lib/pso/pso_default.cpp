1436ba704e304cff4e783a1c704a109da80af9c9
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2021-10-24 04:07:22 +0900

    Initial commit2

---
+++ lib/pso/pso_default.cpp
@@ -0,0 +1,14 @@
+/*
+ * pso_default.cpp
+ *
+ *  Created on: 2021/10/24
+ *      Author: matsu
+ */
+
+#include "pso_default.hpp"
+
+namespace pso_default
+{
+
+}
+
---
+++ lib/pso/pso_default.hpp
@@ -0,0 +1,25 @@
+/*
+ * pso_default.hpp
+ *
+ *  Created on: 2021/10/24
+ *      Author: matsu
+ */
+
+#ifndef PSO_PSO_DEFAULT_HPP_
+#define PSO_PSO_DEFAULT_HPP_
+
+namespace pso_default
+{
+	struct Particle
+	{
+	public:
+		Particle();
+		virtual ~Particle();
+	};
+
+
+}
+
+
+
+#endif /* PSO_PSO_DEFAULT_HPP_ */
